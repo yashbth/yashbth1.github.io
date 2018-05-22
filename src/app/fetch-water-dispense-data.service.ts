4f427a65e6ee41bbff24583f3c5230f7ae07a90e
Yash Baheti <yashbth@iitk.ac.in>
2018-05-22 17:35:48 +0530

    changed php file path

--- src/app/fetch-water-dispense-data.service.ts
+++ src/app/fetch-water-dispense-data.service.ts
@@ -9,7 +9,7 @@ let headers = new HttpHeaders().set('Content-Type', 'application/json; charset=U
 })
 export class FetchWaterDispenseDataService {
 
-  private url = 'http://localhost:8080/';
+  private url = 'http://localhost/~yashbahetiiitk/swajal_dashboard/src/assets/Php/';
 
   
   constructor(private http : HttpClient) {
